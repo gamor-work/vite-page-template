c92d79891e237c0822a67953a6933914c6aa4405
duya <18795905365@163.com>
2021-07-15 12:06:37 +0800

    feat: 1. 初始化界面模版

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Demo from './src'
+import './index.less'
+import 'antd/dist/antd.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Demo />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './index.module.less';
+
+export default () => {
+  return <div>区块</div>
+};
